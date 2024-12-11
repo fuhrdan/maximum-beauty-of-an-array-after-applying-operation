b215d3309a01964aa74384437de67239494c45ee
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2024-12-11 08:10:15 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿//*****************************************************************************
+//** 2779. Maximum Beauty of an Array After Applying Operation      leetcode **
+//*****************************************************************************
+
+int compare(const void *a, const void *b)
+{
+    return (*(int *)a - *(int *)b);
+}
+
+int maximumBeauty(int *nums, int numsSize, int k)
+{
+    qsort(nums, numsSize, sizeof(int), compare);
+
+    int left = 0;
+    int max_beauty = 0;
+
+    for (int right = 0; right < numsSize; right++)
+    {
+        while (nums[right] - nums[left] > 2 * k)
+        {
+            left++;
+        }
+        max_beauty = (right - left + 1 > max_beauty) ? (right - left + 1) : max_beauty;
+    }
+
+    return max_beauty;
+}
